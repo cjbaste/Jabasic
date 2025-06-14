6cfb0737b06a23723185fb3b0643607569f443de
cjbaste <93541688+cjbaste@users.noreply.github.com>
2025-06-14 01:09:00 +0000

    added comment

--- jabasics.java
+++ jabasics.java
@@ -2,6 +2,14 @@ public class jabasics {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //this is a single line comment
+        /*this
+         * is
+         * a 
+         * multi-line
+         * comment
+         */
     }
 }
+
 
